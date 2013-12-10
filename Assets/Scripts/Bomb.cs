ae6ca44e677053a585dca1511beb60ad68be561f
yanyshynv <yanyshynv@gmail.com>
2013-12-10 09:27:02 +0200

    Release version 0.4

---
+++ Assets/Scripts/Bomb.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bomb : MonoBehaviour {
+	
+	public int speed = 40;
+	public int power = 10;
+	public AudioClip boom1;
+	public AudioClip boom2;
+	public AudioClip empire;
+	public string enemy_tag = "Player";
+	public Vector3 player_point;
+	void Update () {
+		transform.position=Vector3.MoveTowards (transform.position, player_point, speed * Time.deltaTime);
+		StartCoroutine(SelfDestroy());
+		Vector3 direction = player_point - transform.position;
+		float distance = direction.magnitude;
+		if (distance < 0.5f) {
+			GameObject bang = Instantiate(Resources.Load("Boom")) as GameObject;
+			bang.transform.position = transform.position;
+			Destroy(gameObject);
+		}
+	}
+	
+	IEnumerator SelfDestroy()	{
+		for (float timer = 4; timer >= 0; timer -= Time.deltaTime)
+			yield return 0;
+		Destroy(gameObject);
+	}
+	
+	void OnTriggerEnter(Collider target){
+		if (target.gameObject.tag == enemy_tag) {
+			giveDamage(target.gameObject);
+			Destroy (gameObject);
+		}
+	}
+	
+	void giveDamage(GameObject target){
+		Player pl = target.gameObject.GetComponent<Player> ();
+		if (pl != null) {
+			GameSettings.health-=power;
+			if(GameSettings.health<=0){
+				GameSettings.health=0;
+				GameSettings.u_r_dead=true;
+				AudioSource.PlayClipAtPoint(boom2,transform.position);
+				AudioSource.PlayClipAtPoint(empire,transform.position);
+				Destroy (target.gameObject);
+			}
+			else{
+				AudioSource.PlayClipAtPoint(boom1,transform.position);
+			}
+		}
+		GameObject bang = Instantiate(Resources.Load("Boom"))as GameObject;
+		bang.transform.position = new Vector3(transform.position.x,target.transform.position.y+5,(target.transform.position.z+transform.position.z)/2);
+		Debug.Log("Destroy "+enemy_tag);
+	}
+}
---
+++ Assets/Scripts/EnemyBomb.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyBomb : MonoBehaviour {
+	
+	public int health = 50;
+	public int ship_speed = 30;
+	public int fire_speed = 40;
+	public float fire_frequency;
+	private bool in_position = false;
+	public Vector3 start_point;
+	private Vector3 move_point;
+	public AudioClip fire;
+	public AudioClip flying;
+	
+	void Start(){
+		fire_frequency = Random.Range (1f, 4f);
+		StartCoroutine(FireLaser());
+		AudioSource.PlayClipAtPoint(flying,transform.position,0.6f);
+		//start_point=new Vector3(Random.Range(-9, 9)*10, 5, Random.Range(5, 15)*10);
+	}
+	
+	void Update () {
+			MovementAlgoritm ();
+	}
+	
+	void OnCollisionEnter (Collision col){
+		Debug.Log (col.gameObject.name);
+	}
+	
+	IEnumerator FireLaser()	{
+		while (true){
+			for (float timer = 0; timer < fire_frequency; timer += Time.deltaTime)
+				yield return 0;
+			Laser();
+			fire_frequency = Random.Range (1f, 4f);
+		}
+	}
+	
+	void Laser(){
+		GameObject pl = GameObject.Find("Player");
+		if(pl!=null){
+			GameObject bullet = Instantiate(Resources.Load("Bomb"))as GameObject;
+			bullet.transform.position = transform.position;
+			Bomb bt = bullet.GetComponent<Bomb>();
+			bt.player_point = pl.transform.position;
+			bt.player_point = pl.transform.position;
+			AudioSource.PlayClipAtPoint(fire,transform.position,0.6f);
+		}
+	}
+	
+	void MovementAlgoritm(){
+		if (!in_position) {
+			transform.position = Vector3.MoveTowards (transform.position, start_point, ship_speed * Time.deltaTime);
+			Vector3 direction = start_point - transform.position;
+			float distance = direction.magnitude;
+			if (distance < 0.5f) {
+				in_position = true;
+				ship_speed=10;
+				move_point = new Vector3(Random.Range(-9, 9)*10, 5, Random.Range(5, 13)*10);
+			}
+		}
+		else{
+			transform.position = Vector3.MoveTowards (transform.position, move_point, ship_speed * Time.deltaTime);
+			Vector3 direction = move_point - transform.position;
+			float distance = direction.magnitude;
+			if (distance < 0.5f) {
+				move_point = new Vector3(Random.Range(-9, 9)*10, 5, Random.Range(5, 13)*10);
+			}
+
+		}
+	}
+}
